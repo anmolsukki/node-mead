85c3f31f41946ab1a4b39d37a6042e3bb6bbd23f
anmolsukki <anmolsukki94@gmail.com>
2020-07-29 01:35:11 +0530

    static file

---
+++ sections/section-7/1_web_server/README.md
@@ -0,0 +1,6 @@
+<h4>run url:-</h4>
+
+1. http://localhost:3000
+2. http://localhost:3000/index.html
+3. http://localhost:3000/help
+4. http://localhost:3000/about
--- sections/section-7/1_web_server/src/app.js
+++ sections/section-7/1_web_server/src/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+const publicDirPath = path.join(__dirname, "../public")
+app.use(express.static(publicDirPath))
 
 app.get("", (req, res) => {
     res.send("Hello Express!")
 });
 
 app.get("/help", (req, res) => {
-    res.send("Help Page!")
+    res.send("<h1>Help Page!</h1>")
+})
+
+app.get("/about", (req, res) => {
+    res.send({
+        name: "Anmol Kumar Singh",
+        Course: "Node JS"
+    })
 })
 
 app.listen(3000, () => {
