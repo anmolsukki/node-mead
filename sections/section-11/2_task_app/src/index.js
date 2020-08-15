ff6514c722a07e9abe2bbc555a1551bf7b5df794
anmolsukki <anmolsukki94@gmail.com>
2020-08-16 01:09:26 +0530

    seprate mongoose file

---
+++ sections/section-11/2_task_app/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post("/users", (req, res) => {
+    res.send("testing")
+})
+
+app.listen(port, () => {
+    console.log("Server is up on port", 3000)
+})
